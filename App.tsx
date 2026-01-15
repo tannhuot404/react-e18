ba1b748a8b3ad3cee39f5b1bb97200be22cb98e0
Tannhuot KHOUV <khouvtannhuot168@gmail.com>
2026-01-15 18:39:15 +0700

    basic ui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  Pressable,
+  TouchableOpacity,
+  TextInput,
+  Modal,
+} from "react-native";
+
+import { useState } from "react";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const imgPressed = () => {
+    console.log("Image Pressesd");
+  };
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [modalVisible, setmodalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Pressable onPress={imgPressed}>
+        <Image
+          style={styles.imgStyle}
+          source={require("./assets/adaptive-icon.png")}
+        ></Image>
+      </Pressable>
+
+      <TouchableOpacity onPress={imgPressed}>
+        <Image
+          style={styles.imgStyle}
+          source={{ uri: "https://picsum.photos/200" }}
+        />
+      </TouchableOpacity>
+
+      <Button
+        title="Login"
+        onPress={() => {
+          console.log("login.");
+        }}
+      /> */}
+
+      <Image
+        style={styles.imgStyle}
+        source={{ uri: "https://picsum.photos/200" }}
+      />
+
+      <View style={styles.textInputContainer}>
+        <Text>Username: </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter username"
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
+
+      <View style={styles.textInputContainer}>
+        <Text>Password: </Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+        ></TextInput>
+      </View>
+      <Button
+        title="Login"
+        onPress={() => {
+          if (username === 'admin' && password === '123'){
+            setmodalVisible(true);
+          } else {
+            Alert.alert("Wrong username or password.")
+          }
+        }}
+      />
+      <Text>
+        Don't have account?{" "}
+        <Text
+          onPress={() => {
+            Alert.alert("register");
+          }}
+          style={styles.registerTextStyle}
+        >
+          Register
+        </Text>
+      </Text>
+
+      <Modal visible={modalVisible} animationType="slide">
+        <Button title="Close" onPress={()=>{setmodalVisible(false)}}/>
+      </Modal>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 25,
+    gap: 30,
+  },
+  registerTextStyle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+    color: "red",
+  },
+  imgStyle: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+    gap: 30,
+    alignItems: "baseline",
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 5,
+    height: 40,
   },
 });
